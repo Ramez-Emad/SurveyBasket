f772792279f36d76e02b3b947442c151d88da2d4
Ramez <ramezemad44@gmail.com>
2025-07-14 13:03:14 +0300

    Add RegisterRequest and its Validtor

---
+++ Shared/Abstractions/Consts/RegexPatterns.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Abstractions.Consts;
+public static class RegexPatterns
+{
+    public const string Password = "(?=(.*[0-9]))(?=.*[\\!@#$%^&*()\\\\[\\]{}\\-_+=~`|:;\"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}";
+}
